6a664a5f1036d33f4546a806937e14c733fab23a
Nicolas Le Novère <n.lenovere@gmail.com>
2018-10-08 18:10:16 +0100

    Added GPLv3 licence header

--- plotGODESeq.R
+++ plotGODESeq.R
@@ -1,3 +1,19 @@
+# plotGODESeq
+# Copyright (C) 2018 Nicolas Le Novère
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 # This function aims at integrating Gene Ontology overepresentation analysis and differential gene expression.
 
 plotGODESeq <- function(goenrich,
